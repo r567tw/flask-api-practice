4bf0466f9936f4a3cafab07b35eaccf0ad2b8a3d
r567tw <r567tw@gmail.com>
2022-10-22 11:45:16 +0800

    marshmallow

--- resources/item.py
+++ resources/item.py
@@ -2,42 +2,50 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from db import items
-from flask_smorest import abort,Blueprint
+from flask_smorest import abort, Blueprint
+
+from schemas import ItemSchema, ItemUpdateSchema
 
 
-item = Blueprint("items",__name__,description="Item")
+item = Blueprint("items", __name__, description="Item")
+
 
 @item.route("/items/<string:id>")
 class Item(MethodView):
-    def put(self,id):
-        data = request.get_json()
-        
+    @item.arguments(ItemUpdateSchema)
+    @item.response(200, ItemSchema)
+    def put(self, data, id):
+        # data = request.get_json()
+
         try:
             items[id] |= data
             return data
         except KeyError:
-            abort(404,message="Item not found")
+            abort(404, message="Item not found")
 
-    def delete(self,id):      
+    def delete(self, id):
         try:
             result = {"deleted": True, **items[id]}
             del items[id]
             return result
         except KeyError:
-            abort(404,message="Item not found")
+            abort(404, message="Item not found")
 
 
 @item.route("/items")
 class ItemList(MethodView):
+    @item.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return list(items.values())
 
-    def post(self):
-        data = request.get_json()
+    @item.arguments(ItemSchema)
+    @item.response(201, ItemSchema)
+    def post(self, data):
+        # data = request.get_json()
         id = uuid.uuid4().hex
-        item = {"id": id,**data}
+        item = {"id": id, **data}
         try:
             items[id] = item
             return item
         except KeyError:
-            abort(404,message="Item not found")
+            abort(404, message="Item not found")
--- resources/store.py
+++ resources/store.py
@@ -2,30 +2,34 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from db import stores
-from flask_smorest import abort,Blueprint
+from flask_smorest import abort, Blueprint
+
+from schemas import StoreSchema
 
 
-store = Blueprint("stores",__name__,description="Store")
+store = Blueprint("stores", __name__, description="Store")
+
 
 @store.route("/stores/<string:id>")
 class Store(MethodView):
-
-
-    def get(self,id):
+    @store.response(200, StoreSchema)
+    def get(self, id):
         try:
             return stores[id]
         except KeyError:
-            abort(404,message = "Store is not found")
+            abort(404, message="Store is not found")
 
 
 @store.route("/stores")
 class StoreList(MethodView):
-    def post(self):
-        data = request.get_json()
+    @store.arguments(StoreSchema)
+    @store.response(201, StoreSchema)
+    def post(self, data):
         store_id = uuid.uuid4().hex
-        newstore = {**data,"id": store_id}
+        newstore = {**data, "id": store_id}
         stores[store_id] = newstore
         return newstore, 201
-        
+
+    @store.response(200, StoreSchema(many=True))
     def get(self):
         return {"stores": list(stores.values())}
